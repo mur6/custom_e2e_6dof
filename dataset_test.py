9da8a0405a15aad094e81f3c8899226d8aeced43
MURAKI Taichi <muraki.taichi@gmail.com>
2024-04-03 15:22:09 +0900

    データセットのテスト.

--- dataset_test.py
+++ dataset_test.py
@@ -20,15 +20,26 @@ from pose_cnn import (
     RotationBranch,
 )
 
+from utils import PROPSPoseDataset
+import utils
+
 # vgg16 = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
 # feature_extractor = FeatureExtraction(pretrained_model=vgg16)
 # segmentation_branch = SegmentationBranch()
 # translation_branch = TranslationBranch()
 # rotation_branch = RotationBranch()
 
+
+def get_data():
+    PATH = "."
+    train_dataset = PROPSPoseDataset(PATH, "train")
+    val_dataset = PROPSPoseDataset(PATH, "val")
+    return train_dataset, val_dataset
+
+
 def main():
-    pass
+    get_data()
+
 
 if __name__ == "__main__":
     main()
-
